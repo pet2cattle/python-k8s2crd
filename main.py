907c92c45d4ccdced395a9ba0be172bc8d18bb79
Jordi Prats <jordi.prats@gmail.com>
2022-10-12 19:09:02 +0200

    block try-catch

--- main.py
+++ main.py
@@ -67,61 +67,61 @@ try:
   
   input = pureyaml.loads(input_str)
 
-  if len(sys.argv) != 2:
-    candidate_ag_w_ver = input['apiVersion']
-  else:
-    candidate_ag_w_ver = sys.argv[1]
-
-  tk_apigroup =candidate_ag_w_ver.split('/')
-
-  if len(tk_apigroup)>1:
-    api_group=tk_apigroup[0]
-    api_version=tk_apigroup[1]
-  else:
-    api_group=tk_apigroup[0]
-    api_version="v1"
-
-  output = """apiVersion: apiextensions.k8s.io/v1
-kind: CustomResourceDefinition
-metadata:
-  name: """+input['kind'].lower()+"""s."""+api_group+"""
-spec:
-  group: """+api_group+"""
-  names:
-    kind: """+input['kind']+"""
-    plural: """+input['kind'].lower()+"""s
-  scope: Namespaced
-  versions:
-  - name: """+api_version+"""
-    served: true
-    storage: true
-    schema:
-      openAPIV3Schema:
-        type: object"""
-  found_element = False
-  depth = 0
-  for key in input:
-    if key in [ 'status', 'apiVersion', 'kind', 'metadata']:
-      continue
-    if key == "spec":
-      input[key].pop('finalizers', None)
-
-    if type(input[key]) is dict and len(input[key].keys()) == 0:
-      continue
-
-    if not found_element:
-      found_element = True
-      output += """
-        properties:
-"""
-    output = add_element(key, input[key], depth, output)
-
-  print(output)
-
 except Exception as e:
   output = ""
   exc_type, exc_obj, exc_tb = sys.exc_info()
   fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
   print(exc_type, fname, exc_tb.tb_lineno)
   sys.exit("Error: "+str(e))
+
+if len(sys.argv) != 2:
+  candidate_ag_w_ver = input['apiVersion']
+else:
+  candidate_ag_w_ver = sys.argv[1]
+
+tk_apigroup =candidate_ag_w_ver.split('/')
+
+if len(tk_apigroup)>1:
+  api_group=tk_apigroup[0]
+  api_version=tk_apigroup[1]
+else:
+  api_group=tk_apigroup[0]
+  api_version="v1"
+
+output = """apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+name: """+input['kind'].lower()+"""s."""+api_group+"""
+spec:
+group: """+api_group+"""
+names:
+  kind: """+input['kind']+"""
+  plural: """+input['kind'].lower()+"""s
+scope: Namespaced
+versions:
+- name: """+api_version+"""
+  served: true
+  storage: true
+  schema:
+    openAPIV3Schema:
+      type: object"""
+found_element = False
+depth = 0
+for key in input:
+  if key in [ 'status', 'apiVersion', 'kind', 'metadata']:
+    continue
+  if key == "spec":
+    input[key].pop('finalizers', None)
+
+  if type(input[key]) is dict and len(input[key].keys()) == 0:
+    continue
+
+  if not found_element:
+    found_element = True
+    output += """
+      properties:
+"""
+  output = add_element(key, input[key], depth, output)
+
+print(output)
 
